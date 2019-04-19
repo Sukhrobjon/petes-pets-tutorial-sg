cf498968467fdf9c7d1d34cabe007d36b81bfa6b
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-04-19 11:56:13 -0700

    [updated] route/model with AWS image uploader

--- models/pet.js
+++ models/pet.js
@@ -7,17 +7,21 @@ const mongoosePaginate = require('mongoose-paginate');
 mongoosePaginate.paginate.options = {
   	limit: 3 // how many records on each page
 };
+
 const PetSchema = new Schema({
     name: { type: String, required: true }, 
     birthday: {type: String, required: true }, 
     species: { type: String, required: true }, 
     picUrl: { type: String, required: true }, 
     picUrlSq: { type: String, required: true }, 
+    avatarUrl: { type: String, required: true },
     favoriteFood: { type: String, required: true }, 
     description: { type: String, minlength: 140, required: true }
 	}, {
   		timestamps: true
 });
+
+
 
 PetSchema.plugin(mongoosePaginate);
 
